d665c2b89215038a93453111f127d17393e02fe0
FY12345432 <150170084+FY12345432@users.noreply.github.com>
2024-02-28 07:17:05 +0800

    Update DiamondsGitHubPractice.R

--- DiamondsGitHubPractice.R
+++ DiamondsGitHubPractice.R
@@ -1,3 +1,9 @@
 library(tidyverse)
 library(dplyr)
 diamonds
+
+# Subset for variables x, y, z and limit to the first 30 rows
+subset_df <- df[c("x", "y", "z")][1:30,]
+
+# Print the subsetted data frame
+print(subset_df)
